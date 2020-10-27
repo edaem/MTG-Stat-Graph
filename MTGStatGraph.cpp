b21cc8e1d8707c8fa2f4215702a08ef6f58b11b9
Chauncey Meade <cmeade91@gmail.com>
2020-10-27 18:43:31 -0400

    added comments

--- MTGStatGraph.cpp
+++ MTGStatGraph.cpp
@@ -15,30 +15,41 @@ int main(int argc, char* argv[]){
     string deckFileName;
     string line;
     map<string, int> lands, spells;
-    
     int count;
 
+    /* load cards.json */
     in.open("cards.json");
     in >> cards;
     in.close();
 
+    /* exits if there isn't at least 1 argument */
     if(argc < 2){
         exit(1);
     }
 
+    /* opens given decklist file */
     deckFileName = argv[1];
     in.open(deckFileName);
     
+    /* procceses each line of the file */
     while(getline(in, line)){
         stringstream ls, ns;
+
+        /* breaks on a blank line (indicates that the sideboard follows) */
         if(line.length() <= 1)
             break;
+        
+        /* checks if the file is using Window's style line breaks, removes extraneous character if so */
         if(line[line.length() - 1] == '\r'){
             line.erase(line.length() - 1, 1);
         }
         //cout << line << " ";
+
+        /* get the amount of the card in deck */
         ls << line;
         ls >> count;
+
+        /* rest of the line is the card name, extracts it */
         while(!ls.eof()){
             string temp;
             ls >> temp;
@@ -48,10 +59,13 @@ int main(int argc, char* argv[]){
         }
         string name = ns.str();
         string type = cards[name]["type"];
+        /* insert lands into the lands map */
         if(type.find("Land") != string::npos){
             
             lands.insert(pair<string, int>(name, count));
-        } else{
+        } 
+        /* insert spells into the spells map */
+        else{
             cout << name << " is a spell\n";
             spells.insert(pair<string, int>(name, count));
         }
