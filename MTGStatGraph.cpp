baad96d7d44a9e325357e8effce7e6dc4b941560
Chauncey Meade <cmeade91@gmail.com>
2020-10-27 18:22:54 -0400

    Beginnings of reading in decklist. Deck is sorted into two maps, one for lands and one for spells.

--- MTGStatGraph.cpp
+++ MTGStatGraph.cpp
@@ -1,5 +1,61 @@
 #include "json.hpp"
+#include <fstream>
+#include <iostream>
+#include <iomanip>
+#include <map>
+#include <sstream>
+#include <utility>
 
-int main(){
-    printf("test\n");
+using namespace std;
+using json = nlohmann::json;
+
+int main(int argc, char* argv[]){
+    ifstream in;
+    json cards;
+    string deckFileName;
+    string line;
+    map<string, int> lands, spells;
+    
+    int count;
+
+    in.open("cards.json");
+    in >> cards;
+    in.close();
+
+    if(argc < 2){
+        exit(1);
+    }
+
+    deckFileName = argv[1];
+    in.open(deckFileName);
+    
+    while(getline(in, line)){
+        stringstream ls, ns;
+        if(line.length() <= 1)
+            break;
+        if(line[line.length() - 1] == '\r'){
+            line.erase(line.length() - 1, 1);
+        }
+        //cout << line << " ";
+        ls << line;
+        ls >> count;
+        while(!ls.eof()){
+            string temp;
+            ls >> temp;
+            ns << temp;
+            if(!ls.eof())
+                ns << " ";
+        }
+        string name = ns.str();
+        string type = cards[name]["type"];
+        if(type.find("Land") != string::npos){
+            
+            lands.insert(pair<string, int>(name, count));
+        } else{
+            cout << name << " is a spell\n";
+            spells.insert(pair<string, int>(name, count));
+        }
+        //cout << count << " " << cards[name]["convertedManaCost"] << endl;
+    }
+    cout << "Land count: " << lands.size() << endl << "Spell cout: " << spells.size() << endl;
 }
